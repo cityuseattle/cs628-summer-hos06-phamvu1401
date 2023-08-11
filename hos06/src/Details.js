2f84b117e758f93c9f927739e8e1f3afbeb7b2e8
Vu Pham <38738288+phamvu1401@users.noreply.github.com>
2023-08-11 23:05:21 +0000

    Submission for Module 06 - Vu Pham

---
+++ hos06/src/Details.js
@@ -0,0 +1,19 @@
+// Section 2 - Deatils.js
+import React from 'react';
+
+function Details (){
+    let navigate = useNavigate(); 
+    function goFoward() {
+        navigate(1)
+    }
+
+    return <div>
+        <h2>Understanding browser router</h2>
+        <p>
+            In this HOS we are implementing routing using BrowserRouter. <br />
+            You can learn more <a href="https://reactrouter.com/en/main/router-components/browser-router" target="_blank" rel="noopener noreferrer">here</a>
+        </p>
+    </div>
+}
+
+export default Details;
---
+++ hos06/src/Member.js
@@ -0,0 +1,16 @@
+// Section 5 - Member.js
+import React from 'react';
+import { useParams } from 'react-router-dom'
+
+function Member() {
+
+    const { id } = useParams();
+
+    return (
+    <div>
+        <h2>Member Details</h2>
+        <p>Member ID: {id}</p>
+    </div>
+    );
+}
+export default Member;
